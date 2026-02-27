904fb1c4cba00106868a8cbb28e0a846fd780c06
Rudas08 <raulrudas06@gmail.com>
2026-02-27 01:03:53 -0500

    primer commit inventario

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include("conexion.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inventario</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Inventario funcionando 🚀</h1>
+
+<?php
+echo "Conexión exitosa a la base de datos.";
+?>
+
+</body>
+</html>
